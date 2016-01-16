88b5da23736713f6ed413275d650208245ac146e
APS <apai.sukphol@gmail.com>
2016-01-16 14:31:24 +0700

    Bind Widget Finished

--- app/src/main/java/devlbiz/aps/mytraffic/MainActivity.java
+++ app/src/main/java/devlbiz/aps/mytraffic/MainActivity.java
@@ -2,12 +2,29 @@ package devlbiz.aps.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Area for define variant
+    private ListView trafficListView;
+    private Button aboutMeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+    }   //Main Method การทำงานหลัก
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.livTraffic);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
     }
-}
+
+}   //Main Class คลาสหลัก
