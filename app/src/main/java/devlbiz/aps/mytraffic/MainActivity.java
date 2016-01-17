56a75faf55287c31fedb0a9a1159bd2c10702250
APS <apai.sukphol@gmail.com>
2016-01-17 09:38:02 +0700

    Action View Finished

--- app/src/main/java/devlbiz/aps/mytraffic/MainActivity.java
+++ app/src/main/java/devlbiz/aps/mytraffic/MainActivity.java
@@ -1,6 +1,8 @@
 package devlbiz.aps.mytraffic;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -37,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
                 MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),
                         R.raw.effect_btn_shut);
                 buttonMediaPlayer.start();
+
+                //Show WebView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/unU8SdLtd3E"));
+                startActivity(objIntent);
 
             } //Event on Click
         });
